015172d6ccaf98b95f1c6967c04d9c7103ed0401
ShrmAmn <105295671+ShrmAmn@users.noreply.github.com>
2022-09-20 16:57:10 +0530

    Create BracketIndexingAndSelection.py

---
+++ DataAnalysisNumPy/NumPyArray/BracketIndexingAndSelection.py
@@ -0,0 +1,17 @@
+import numpy as np
+#Creating sample array
+arr = np.arange(0,11)
+# array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10])
+
+# ************************ Bracket Indexing and Selection ***************************
+#Get a value at an index
+arr[8]
+# 8
+
+#Get values in a range
+arr[1:5]
+# array([1, 2, 3, 4])
+
+#Get values in a range
+arr[0:5]
+# array([0, 1, 2, 3, 4])
